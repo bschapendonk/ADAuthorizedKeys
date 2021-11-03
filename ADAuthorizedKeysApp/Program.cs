a2d493e6701084fb4f4ffcde6c7ac68e67cfbfe8
Bart Schapendonk <bart@schapendonk.com>
2021-11-03 18:45:58 +0100

    initual import

---
+++ ADAuthorizedKeysApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace ADAuthorizedKeysApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                return;
+
+            var username = args[0];
+            var splittedUsername = username.Split('\\');
+            if (splittedUsername.Length > 1)
+                username = splittedUsername[1];
+
+            splittedUsername = username.Split('@');
+            if (splittedUsername.Length > 1)
+                username = splittedUsername[0];
+
+            var searcher = new DirectorySearcher();
+            searcher.Filter = $"(&(objectClass=user)(sAMAccountName={username}))";
+            searcher.PropertiesToLoad.Add("sshPublicKeys");
+
+            var result = searcher.FindOne();
+            if (result != null)
+            {
+                foreach (string sshPublicKey in result.Properties["sshPublicKeys"])
+                {
+                    Console.Write(sshPublicKey);
+                }
+            }
+        }
+    }
+}
