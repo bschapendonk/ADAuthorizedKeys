c7f951c27c75c6ff80875bc51dda6774c55d2f5d
Bart Schapendonk <bart@schapendonk.com>
2021-11-03 20:48:47 +0100

    sanity checking

--- ADAuthorizedKeysApp/Program.cs
+++ ADAuthorizedKeysApp/Program.cs
@@ -19,6 +19,17 @@ namespace ADAuthorizedKeysApp
             if (splittedUsername.Length > 1)
                 username = splittedUsername[0];
 
+            // https://docs.microsoft.com/nl-nl/windows/win32/adschema/a-samaccountname
+            // SAM-Account-Name attribute
+            // This attribute must be 20 characters or less to support earlier clients, and cannot contain any of these characters:
+            //  "/ \ [ ] : ; | = , + * ? < >
+
+            if (username.Length > 20)
+                return;
+
+            if (username.IndexOfAny(new char[] { '/', '\\', '[', ']', ':', ';', '|', '=', ',', '+', '*', '?', '<', '>' }) != -1)
+                return;
+
             var searcher = new DirectorySearcher();
             searcher.Filter = $"(&(objectClass=user)(sAMAccountName={username}))";
             searcher.PropertiesToLoad.Add("sshPublicKeys");
