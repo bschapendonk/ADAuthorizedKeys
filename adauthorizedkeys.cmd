50e536cb291f3d1d7ccae92be1bbe586838b6d72
Bart Schapendonk <bart@schapendonk.com>
2021-11-04 10:54:57 +0100

    Update adauthorizedkeys.cmd

--- adauthorizedkeys.cmd
+++ adauthorizedkeys.cmd
@@ -1,2 +1,4 @@
-@echo off
-C:\Windows\System32\dsquery.exe * -filter "(&(objectClass=user)(sAMAccountName=%1))" -attr sshPublicKeys -l
+@ECHO OFF
+FOR /F "tokens=2 delims=\\" %%G in ("%1") DO (
+	C:\Windows\System32\dsquery.exe * -filter "(&(objectClass=user)(sAMAccountName=%%G))" -attr sshPublicKeys -l
+)
